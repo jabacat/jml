33e82c428b9a3cb66093bc10931058bfc7424ce8
adamhutchings <adam.abahot@gmail.com>
2023-09-17 22:16:47 -0400

    Add empty class -- the very start!

---
+++ core/include/jml/model.hpp
@@ -0,0 +1,19 @@
+/**
+ * This is the header file with the specifications for any top-level neural net
+ * model. Any useful application of the JML library will most likely include
+ * this header.
+ * Author: Adam Hutchings
+ * 09-17-23
+*/
+
+#pragma once
+
+namespace jml {
+
+class Model {
+
+    
+
+};
+
+}
