4d3e02e8d1e20b7984ca3aa3560cbf4289f04d32
Jake <jakeroggenbuck2@gmail.com>
2023-10-08 18:32:08 -0700

    Log other files too

--- core/src/math/vector.cpp
+++ core/src/math/vector.cpp
@@ -1,9 +1,12 @@
 #include <jml/math/vector.hpp>
+#include <internal/logger.hpp>
 
 #include <cstring>
 #include <iostream>
 
 namespace jml {
+
+Logger *LOGGER = Logger::Instance();
 
 Vector::Vector(int length) {
 
@@ -11,52 +14,48 @@ Vector::Vector(int length) {
     memset(this->entries, 0, length * sizeof(double));
 
     this->length = length;
-
 }
 
-Vector::~Vector() {
-    delete this->entries;
-}
+Vector::~Vector() { delete this->entries; }
 
-void Vector::apply(ActivationFunction * fn) {
+void Vector::apply(ActivationFunction *fn) {
     for (int i = 0; i < this->length; ++i) {
         this->entries[i] = fn->f(this->entries[i]);
     }
 }
 
-void Vector::add(Vector& v) {
+void Vector::add(Vector &v) {
     if (this->length != v.length) {
-        std::cout << "Tried to add a vector of length " << v.length <<
-        "to a vector of length " << this->length << "." << std::endl;
+        LOGGER->log(Log(WARN)
+                    << "Tried to add a vector of length " << v.length
+                    << "to a vector of length " << this->length << ".\n");
     }
     for (int i = 0; i < this->length; ++i) {
         this->entries[i] += v.entries[i];
     }
 }
 
-int Vector::get_size() {
-    return this->length;
-}
+int Vector::get_size() { return this->length; }
 
 double Vector::get_entry(int pos) {
     if (pos < 0 || pos >= this->length) {
-        std::cout << "Position " << pos << " out of bounds." << std::endl;
+        LOGGER->log(Log(WARN) << "Position " << pos << " out of bounds.\n");
     }
     return this->entries[pos];
 }
 
 void Vector::set_entry(int pos, double val) {
     if (pos < 0 || pos >= this->length) {
-        std::cout << "Position " << pos << " out of bounds." << std::endl;
+        LOGGER->log(Log(WARN) << "Position " << pos << " out of bounds.\n");
     }
     this->entries[pos] = val;
 }
 
 void Vector::add_entry(int pos, double val) {
     if (pos < 0 || pos >= this->length) {
-        std::cout << "Position " << pos << " out of bounds." << std::endl;
+        LOGGER->log(Log(WARN) << "Position " << pos << " out of bounds.\n");
     }
     this->entries[pos] += val;
 }
 
-}
+} // namespace jml
